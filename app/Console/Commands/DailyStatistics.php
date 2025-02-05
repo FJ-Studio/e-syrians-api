5c2bc1fb8b4ebc35e89edd81830945800f1f79c3
Feras Jobeir <fjobeir@gmail.com>
2025-02-05 12:46:35 +0300

    refactor DailyStatistics command to use consistent cache keys and add gender statistics caching

--- app/Console/Commands/DailyStatistics.php
+++ app/Console/Commands/DailyStatistics.php
@@ -33,9 +33,9 @@ class DailyStatistics extends Command
         // Get the current date
         $dateKey = Carbon::now()->toDateString();
         // Get the cache key
-        $cacheKey = config('e-syrians.cache.daily_registrants');
+        $usersKey = config('e-syrians.cache.daily_registrants');
         // Get the statistics from the cache
-        $statistics = Cache::get($cacheKey, []);
+        $statistics = Cache::get($usersKey, []);
         // Check if the statistics for the current date are already calculated
         if (!isset($statistics[$dateKey])) {
             $statistics[$dateKey] = [
@@ -43,10 +43,20 @@ class DailyStatistics extends Command
                 'verified' => User::whereDate('verified_at', $dateKey)->count(),
             ];
             // Store the updated statistics permanently
-            Cache::forever($cacheKey, $statistics);
+            Cache::forever($usersKey, $statistics);
             $this->info('Daily users stats have been calculated and cached successfully.');
         }
         // gender stats
         $genderKey = config('e-syrians.cache.gender');
+        Cache::forever(
+            $genderKey,
+            [
+                'male' => User::where('gender', 'm')->whereNull('verified_at')->count(),
+                'female' => User::where('gender', 'f')->whereNull('verified_at')->count(),
+                'verified_male' => User::where('gender', 'm')->whereNotNull('verified_at')->count(),
+                'verified_female' => User::where('gender', 'f')->whereNotNull('verified_at')->count(),
+                'unknown' => User::whereNull('gender')->count(),
+            ]
+        );
     }
 }
