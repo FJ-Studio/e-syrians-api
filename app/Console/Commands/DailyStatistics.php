d210c286deea7a28b19ba00270e4a90b450f3f35
Feras Jobeir <fjobeir@gmail.com>
2025-02-05 17:58:52 +0300

    refactor DailyStatistics command to utilize StatsService for improved statistics calculations and add age statistics caching

--- app/Console/Commands/DailyStatistics.php
+++ app/Console/Commands/DailyStatistics.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Console\Commands;
 
 use App\Models\User;
+use App\Services\StatsService;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
@@ -30,33 +31,8 @@ class DailyStatistics extends Command
      */
     public function handle()
     {
-        // Get the current date
-        $dateKey = Carbon::now()->toDateString();
-        // Get the cache key
-        $usersKey = config('e-syrians.cache.daily_registrants');
-        // Get the statistics from the cache
-        $statistics = Cache::get($usersKey, []);
-        // Check if the statistics for the current date are already calculated
-        if (!isset($statistics[$dateKey])) {
-            $statistics[$dateKey] = [
-                'registered' => User::whereDate('created_at', $dateKey)->count(),
-                'verified' => User::whereDate('verified_at', $dateKey)->count(),
-            ];
-            // Store the updated statistics permanently
-            Cache::forever($usersKey, $statistics);
-            $this->info('Daily users stats have been calculated and cached successfully.');
-        }
-        // gender stats
-        $genderKey = config('e-syrians.cache.gender');
-        Cache::forever(
-            $genderKey,
-            [
-                'male' => User::where('gender', 'm')->whereNull('verified_at')->count(),
-                'female' => User::where('gender', 'f')->whereNull('verified_at')->count(),
-                'verified_male' => User::where('gender', 'm')->whereNotNull('verified_at')->count(),
-                'verified_female' => User::where('gender', 'f')->whereNotNull('verified_at')->count(),
-                'unknown' => User::whereNull('gender')->count(),
-            ]
-        );
+        StatsService::calculateDailyUsersStats();
+        StatsService::calculateGenderStats();
+        StatsService::calculateAgeStats();
     }
 }
--- app/Services/StatsService.php
+++ app/Services/StatsService.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class StatsService
 {
@@ -15,5 +18,69 @@ class StatsService
     public static function getGenderStats(): array
     {
         return Cache::get(config('e-syrians.cache.gender'), []);
+    }
+    public static function calculateDailyUsersStats(): void
+    {
+        // Get the current date
+        $dateKey = Carbon::now()->toDateString();
+        // Get the cache key
+        $usersKey = config('e-syrians.cache.daily_registrants');
+        // Get the statistics from the cache
+        $statistics = Cache::get($usersKey, []);
+        // Check if the statistics for the current date are already calculated
+        if (!isset($statistics[$dateKey])) {
+            $statistics[$dateKey] = [
+                'registered' => User::whereDate('created_at', $dateKey)->count(),
+                'verified' => User::whereDate('verified_at', $dateKey)->count(),
+            ];
+            // Store the updated statistics permanently
+            Cache::forever($usersKey, $statistics);
+        }
+    }
+    public static function calculateGenderStats(): void
+    {
+        // Get the cache key
+        $genderKey = config('e-syrians.cache.gender');
+        Cache::forever(
+            $genderKey,
+            [
+                'unverified_m' => User::where('gender', 'm')->whereNull('verified_at')->count(),
+                'unverified_f' => User::where('gender', 'f')->whereNull('verified_at')->count(),
+                'verified_m' => User::where('gender', 'm')->whereNotNull('verified_at')->count(),
+                'verified_f' => User::where('gender', 'f')->whereNotNull('verified_at')->count(),
+                'unknown' => User::whereNull('gender')->count(),
+            ]
+        );
+    }
+    public static function calculateAgeStats(): void
+    {
+        $ageGroups = [
+            '1-15' => [1, 15],
+            '16-30' => [16, 30],
+            '31-45' => [21, 45],
+            '46-60' => [46, 60],
+            '61-75' => [61, 75],
+            '76-90' => [76, 90],
+            '91+' => [61, 200],
+        ];
+
+        $ageStatistics = [
+            'verified' => [],
+            'unverified' => [],
+        ];
+
+        foreach ($ageGroups as $label => [$min, $max]) {
+            $ageStatistics['verified'][$label] = User::whereBetween(
+                DB::raw('TIMESTAMPDIFF(YEAR, birth_date, CURDATE())'),
+                [$min, $max]
+            )->where('verified', true)->count();
+
+            $ageStatistics['unverified'][$label] = User::whereBetween(
+                DB::raw('TIMESTAMPDIFF(YEAR, birth_date, CURDATE())'),
+                [$min, $max]
+            )->where('verified', false)->count();
+        }
+
+        Cache::forever('user_age_statistics', $ageStatistics);
     }
 }
