34a91dba45c2a9837f3636af76ff35890e9965dd
Feras Jobeir <fjobeir@gmail.com>
2025-02-12 02:02:19 +0300

    feat: update login method to return user resource and token in response

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -129,9 +129,8 @@ class UserController extends Controller
 
         if ($user && Hash::check($password, $user->password)) {
             return ApiService::success([
-                'test' => $user,
-                // 'user' => new UserResource($user),
-                // 'token' => explode('|', $user->createToken(date('YYYY-mm-dd-H:i:s'))->plainTextToken)[1],
+                'user' => new UserResource($user),
+                'token' => explode('|', $user->createToken(date('YYYY-mm-dd-H:i:s'))->plainTextToken)[1],
             ]);
         }
         return ApiService::error(401);
