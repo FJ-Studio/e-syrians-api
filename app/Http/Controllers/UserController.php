38db9c1dfdbc8376cdf0213ddc6fd03129826b8a
Feras Jobeir <fjobeir@gmail.com>
2025-02-15 02:45:19 +0300

    feat: include trashed polls in the user's polls retrieval

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -249,7 +249,7 @@ class UserController extends Controller
     public function my_polls(Request $request)
     {
         $user = $request->user();
-        $polls = $user->polls()->with(['options'])->get();
+        $polls = $user->polls()->withTrashed()->with('options')->get();
         return ApiService::success($polls);
     }
 }
