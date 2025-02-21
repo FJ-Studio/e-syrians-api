ceff457bba56c9e02e14446b5aee26c44d9e8eb8
Feras Jobeir <fjobeir@gmail.com>
2025-02-22 02:09:45 +0300

    fix: remove test route for UserAccountVerified email functionality

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
-use App\Mail\UserAccountVerified;
-use App\Models\User;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('/test', function () {
-    $user = User::find(1);
-    $targetUser = User::find(18);
-    $sent = Mail::to($targetUser)->send(new UserAccountVerified($targetUser));
-    dd($user, $targetUser, $sent);
 });
