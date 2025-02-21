3d1bfa8c45184f69cec1425a1983c986ecb8f329
Feras Jobeir <fjobeir@gmail.com>
2025-02-22 00:51:14 +0300

    fix: remove UserRegistered event and add test route for email verification

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Mail\UserReceivedVerification;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('welcome');
-})->name('login');
+Route::get('/test', function () {
+    $user  = User::find(1);
+    $targetUser = User::find(18);
+    $sent = Mail::to('test@example.com')->send(new UserReceivedVerification($user, $targetUser));
+    dd($user, $targetUser, $sent);
+});
