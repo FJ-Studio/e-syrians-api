b50a2d751b3e1b74d5df9a6cf84e5e07f445f84e
Feras Jobeir <fjobeir@gmail.com>
2025-02-22 01:45:21 +0300

    fix: update email header to use anchor tag for improved link functionality

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,12 @@
 @props(['url'])
 <tr>
 <td class="header">
-<span style="display: inline-block;">
+<a href="{{env('FRONTEND_URL')}}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
-</span>
+</a>
 </td>
 </tr>
