df3f230c84a8d8d97fd7257cd80b975a9cd845f7
Feras Jobeir <fjobeir@gmail.com>
2025-04-01 02:09:58 +0300

    feat: update email templates and add localization for account verification messages

---
+++ resources/lang/ar/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'verified_title' => 'تهانينا! تم التحقق من حسابك. 🥳',
+    'verified_body' => 'لقد حصلت على عدد كافٍ من التأكيدات . يمكنك الآن المشاركة بخدمات المنصة كاملة.',
+    'go_to_profile' => 'اذهب إلى ملفي الشخصي',
+    'verification_received_title' => 'قام أحدهم بتأكيد بياناتك',
+    'verification_received_body' => 'تهانينا، لقد قام  :name :surname بتأكيد بيانات حسابك',
+    'review_profile_status' => 'مراجعة حالة الملف الشخصي',
+    'thanks' => 'شكرًا،',
+];
---
+++ resources/lang/ku/mail.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'verified_title' => 'Pîrozbahî! Hesabê te hat testkirin. 🥳',
+    'verified_body' => 'Tu ji civakê yeterî testkirinên berhev kirî. Nûha dikarî bi temamî taybetmendiyên profîla testkirî bihêvî bî.',
+    'go_to_profile' => 'Herin profîla min',
+    'verification_received_title' => 'Testkirinê hat wergirtin',
+    'verification_received_body' => 'Daneyên profîla te hatine testkirin ji hêla :name :surname.',
+    'review_profile_status' => 'Rewşa profîlê kontrol bike',
+    'thanks' => 'Spas,',
+];
